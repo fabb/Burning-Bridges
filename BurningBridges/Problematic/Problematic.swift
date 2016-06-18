003bb5d876d0fb2bfd39228e337bb73d3141fa33
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2016-06-18 12:22:43 +0200

    added comment explaining purpose

--- BurningBridges/Problematic/Problematic.swift
+++ BurningBridges/Problematic/Problematic.swift
@@ -1,6 +1,11 @@
 import Foundation
 import UIKit
 
+
+/**
+	This class demonstrates a few problems that happen with the Swift 3 migrator and bridge in Xcode 8
+	Tested with Xcode 8 beta 1
+*/
 class Problematic {
 
 	// ObjC method should be bridged to `problematic(launchedDueToDeepLink: Bool)`, but gets bridged to `problematicWithLaunchedDue(toDeepLink: Bool)`
